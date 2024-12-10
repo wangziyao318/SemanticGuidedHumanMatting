c9893317690fad7024981ceb905b84f5c23fcde6
Ziyao Wang <ziyaowang318@gmail.com>
2024-12-11 04:15:34 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 3'
     ],
-    include_package_data=False,
+    include_package_data=True,
     packages=find_packages(),
     install_requires = install_requires
 )
